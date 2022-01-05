cd10b5e22eb71ae7c52977af42af6f8a78354160
firdodev <firdeuskasaj@gmail.com>
2022-01-05 15:43:46 +0100

    Firdo

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function checklogin(){
-    if(document.getElementById("email").value == "" && document.getElementById("password").value == ""){
+    if(document.getElementById("email").value == "" || document.getElementById("password").value == ""){
         document.getElementById("error-msg").innerHTML = "Email or Password is not correct !";
         return false;
     }else{
--- signup.js
+++ signup.js
@@ -1,5 +1,5 @@
 function checksignup(){
-    if(document.getElementById("email").value == "" && document.getElementById("password").value == "" && document.getElementById("first-name").value == "" && document.getElementById("last-name").value == ""){
+    if(document.getElementById("email").value == "" || document.getElementById("password").value == "" || document.getElementById("first-name").value == "" || document.getElementById("last-name").value == ""){
         document.getElementById("error-msg").innerHTML = "Your application is not fill correct !";
         return false;
     }else{
